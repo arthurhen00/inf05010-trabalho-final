ee56b30e3dfa0a95d137967d81419efbda7bde5a
Gabriel Pereira <gabipereira8282@gmail.com>
2024-01-17 13:06:51 -0300

    Heuristica pronta

---
+++ src/GRASP.py
@@ -0,0 +1,142 @@
+from copy import deepcopy
+from typing import List
+from math import ceil
+from random import randint, seed
+import time
+
+
+file_name = 'teste2'
+file_path = f'selected_bpp_instances/{file_name}.txt'
+
+
+class Solution():
+    def __init__(self, problem_instance: (int,int,List[int]), assignment:List[List[int]]):
+        self.assignment = assignment
+        self.problem_instance = problem_instance
+        
+    def __hash__(self):
+        return hash(str(self.assignment))
+
+        
+    def __eq__(self, other):
+        if not isinstance(other, Solution):
+            return False
+
+        return (
+            self.assignment == other.assignment
+        )
+        
+    def __lt__(self,other:'Solution'):
+        return  self.get_bin_amount() < other.get_bin_amount()
+    
+    def __le__(self,other:'Solution'):
+        return self.get_bin_amount() <= other.get_bin_amount()
+    
+    def __deepcopy__(self, memo):
+        cls = self.__class__
+        result = cls.__new__(cls)
+        memo[id(self)] = result
+        for k, v in self.__dict__.items():
+            setattr(result, k, deepcopy(v, memo))
+        return result
+    def __str__(self):
+        return f'{self.assignment}'
+    
+    def get_empty_space(self) -> int:
+        _, bin_capacity, weights = self.problem_instance
+        empty_space = 0
+        for bin in self.assignment:
+            empty_space += bin_capacity - sum([weights[x] for x in bin])
+        return empty_space
+    
+    def get_bin_amount(self) -> int:
+        return len(self.assignment)
+    
+    def print_space_remaining(self) -> None:
+        _, bin_capacity, weights = self.problem_instance
+        for bin in self.assignment:
+            print(bin_capacity-sum([weights[x] for x in bin]))
+        return None
+def read_file(file_path: str):
+    weights = []
+    with open(file_path,'r') as f:
+        number_of_items = int(f.readline())
+        bin_capacity = int(f.readline())
+        for i in range(number_of_items):
+            weights.append(int(f.readline()))
+            
+    return number_of_items, bin_capacity, weights
+
+
+def find_initial_solution(problem_instance) -> Solution:
+    number_of_items, bin_capacity, weights = problem_instance
+    assignment = [[]]
+    for i in range(number_of_items):
+        for bin in assignment:
+            if sum([weights[x] for x in bin]) + weights[i] < bin_capacity:
+                bin.append(i)
+            else:
+                assignment.append([i])
+            break
+            
+    return Solution(problem_instance, assignment)
+
+def rand_greedy(solutions: List[Solution],alpha: int) -> Solution:
+    sorted(solutions)
+    chosen_index = randint(0,ceil(len(solutions)/(100/alpha)-1))
+    if solutions[0].get_bin_amount() < solutions[chosen_index].get_bin_amount():
+        return solutions[0]
+    return solutions[chosen_index]
+
+def local_search(solution: Solution) -> List[Solution]:
+    _, bin_capacity , weights = solution.problem_instance
+    neighbors = []
+    for bin_index, bin in enumerate(solution.assignment):
+        for other_bin_index, other_bin in enumerate(solution.assignment):
+            if bin == other_bin:
+                continue
+            other_bin_weight = sum([weights[x] for x in other_bin])
+            for item in bin:
+                if other_bin_weight + weights[item] <= bin_capacity:
+                    new_solution = deepcopy(solution)
+                    new_solution.assignment[other_bin_index].append(item)
+                    new_solution.assignment[bin_index].remove(item)
+                    if new_solution.assignment[bin_index] == []:
+                        new_solution.assignment.remove([])
+                    neighbors.append(new_solution)
+    
+    return neighbors
+
+def grasp(alpha: int,prob_instance, max_iter:int = 100) -> Solution:
+    if alpha > 100 or alpha <= 0:
+        raise Exception('alpha inválido! (0 <= alpha < 100)') from ValueError
+    
+    i = 0
+    explored = set()
+    best_s = find_initial_solution(prob_instance)
+    while i < max_iter:
+        #print(best_s)
+        s = local_search(best_s)
+        if not s:
+            break
+        s = rand_greedy(s, alpha)
+        
+        if s <= best_s and s not in explored:
+            #explored.add(best_s)
+            best_s = s
+        
+        i = i+1
+        
+        
+    return best_s
+
+instance = read_file(file_path)
+num_items, bin_capacity, weights = instance
+print(f'Número de itens: {num_items}')
+print(f'Capacidade dos cestos: {bin_capacity}')
+print(f'peso dos itens: {weights}')
+
+start_time = time.time()
+s = grasp(10,instance)
+print(f'Solução: {s}')
+print("--- %s seconds ---" % (time.time() - start_time))
--- src/bpp_formulation.jl
+++ src/bpp_formulation.jl
@@ -1,6 +1,8 @@
 using JuMP
 using GLPK
 
+
+
 function read_file(file_name)
     # all numbers in file
     data = readlines(file_name)
@@ -14,55 +16,55 @@ function read_file(file_name)
     return num_items, basket_capacity, item_weight
 end
 
-if length(ARGS) != 1
-    println("usage: julia bpp_fomulation.jl <./file/path/file_name>")
-    exit(1)
-end
+
 
 # Caminho relativo ao current work dir
-num_items, basket_capacity, item_weight = read_file(ARGS[1])
+num_items, basket_capacity, item_weight = read_file("./1002_80000_NR_27.txt")
+
+num_bins = num_items
 
 println("Quantidade de números: ", num_items)
 println("Capacidade das cestas: ", basket_capacity)
 println("Pesos: ", item_weight)
 
-m = Model(GLPK.Optimizer)
+m = Model(GLPK.Optimizer;  add_bridges = false)
 
 # var decisao: x[i, j] indica se o item i está na cesta j
-@variable(m, x[1:num_items, 1:num_items], Bin)
+@variable(m, x[1:num_items, 1:num_bins], Bin)
 # indica se pelo menos um item foi atribuído na cesta j
 @variable(m, y[1:num_items], Bin)
 
+
+
+@constraint(m,[i = 1:num_items, j = 1:num_bins] , x[i,j] <= y[j])
+
+
 # Todos itens devem ser usados uma vez (soma das linhas)
-for i in 1:num_items
-    @constraint(m,
-                sum(x[i, j] for j in 1:num_items)
-                == 1)
-end
+@constraint(m, [i = 1:num_items], sum(x[i, j] for j in 1:num_bins)== 1)
 
 # Limite de peso por cesta (soma das colunas)
-for j in 1:num_items
-    @constraint(m,
-                sum(item_weight[i] * x[i, j] for i in 1:num_items)
-                <= basket_capacity * y[j])
-end
+@constraint(m, [j = 1:num_bins] ,sum(item_weight[i] * x[i, j] for i in 1:num_items) <= basket_capacity * y[j])
+
 
 @objective(m, Min, sum(y))
 optimize!(m)
 
-# mostra a matriz de item x cesta
-#@show JuMP.value.(x)
-# mostra as cestas com algum valor
-#@show JuMP.value.(y)
+@show objective_value(m)
+@show JuMP.solve_time(m)
 
-println("Número mínimo de cestos necessários:", objective_value(m)) 
 allocation = value.(x)
+
+
+
 println("Id do item e peso em cada cesto:")
-for j in 1:num_items
+for j in 1:num_bins
     items_in_basket = findall(i -> allocation[i, j] > 0, 1:num_items)
     total_weight = isempty(items_in_basket) ? 0 : sum(item_weight[i] for i in items_in_basket)
     
     if (!isempty(items_in_basket)) 
         println("Cesto $j: Itens $items_in_basket, Peso total: $total_weight")
     end
+
 end
+
+
